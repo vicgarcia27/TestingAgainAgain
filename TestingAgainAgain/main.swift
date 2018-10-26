c873e459049d2dc94bce5b00eaf3b6b51fc757fb
Christian H. Lopez <HOCN@lopezs-macbook-pro.local>
2018-10-26 23:29:14 +0800

    Initial Commit

---
+++ TestingAgainAgain/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestingAgainAgain
+//
+//  Created by Christian H. Lopez on 26/10/2018.
+//  Copyright © 2018 Christian H. Lopez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
